52f21eb7d48c1844042ca920e97fc2a6a995ac43
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2018-04-30 13:32:06 -0700

    Update exception error messages

--- dwave/cloud/exceptions.py
+++ dwave/cloud/exceptions.py
@@ -1,35 +1,35 @@
 class ConfigFileError(Exception):
-    """Base exception for all config file processing errors."""
+    """Base exception for all configuration file processing errors."""
 
 class ConfigFileReadError(ConfigFileError):
-    """Non-existing or unreadable config file specified or implied."""
+    """Non-existing or unreadable configuration file specified or implied."""
 
 class ConfigFileParseError(ConfigFileError):
-    """Invalid format of config file."""
+    """Invalid format of configuration file."""
 
 
 class SolverError(Exception):
     """Generic base class for all solver-related errors."""
 
 class SolverFailureError(SolverError):
-    """An exception raised when there is a remote failure calling a solver."""
+    """Remote failure when calling a solver."""
 
 class SolverAuthenticationError(SolverError):
-    """An exception raised when there is an authentication error."""
+    """Authentication error when calling a solver."""
 
     def __init__(self):
         super(SolverAuthenticationError, self).__init__("Token not accepted for that action.")
 
 class UnsupportedSolverError(SolverError):
-    """The solver we received from the API is not supported by the client."""
+    """Solver received from the API is unsupported by the client."""
 
 
 class CanceledFutureError(Exception):
-    """An exception raised when code tries to read from a canceled future."""
+    """Attempted read from a canceled Future object."""
 
     def __init__(self):
         super(CanceledFutureError, self).__init__("An error occurred reading results from a canceled request")
 
 
 class InvalidAPIResponseError(Exception):
-    """Raised when an invalid/unexpected response from D-Wave Solver API is received."""
+    """Invalid/unexpected response from D-Wave Solver API received."""
